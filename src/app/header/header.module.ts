95f7ec4879912cd8ecd50a701ed4635cd22d126e
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-01-21 01:18:36 +0300

    complete header page

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,22 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 const BASE_MODULES = [
+  RouterModule,
   ReactiveFormsModule,
   CommonModule
 ];
 const COMPONENTS = [HeaderComponent];
+const MATERIAL_MODULES = [MatIconModule];
 
 @NgModule({
   declarations: [
     ...COMPONENTS
   ],
   imports: [
+    ...MATERIAL_MODULES,
     ...BASE_MODULES
   ],
   exports: [
+    ...MATERIAL_MODULES,
     ...BASE_MODULES,
     ...COMPONENTS
   ]
