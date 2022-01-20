48343e5f1258949ba7a3d1ca3a46c8a36bdac8ff
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-01-21 01:19:49 +0300

    complete home page

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
@@ -13,15 +15,21 @@ const BASE_MODULES = [
   CommonModule
 ];
 const COMPONENTS = [HomeComponent];
+const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatCardModule
+];
 
 @NgModule({
   declarations: [
     ...COMPONENTS
   ],
   imports: [
+    ...MATERIAL_MODULES,
     ...BASE_MODULES
   ],
   exports: [
+    ...MATERIAL_MODULES,
     ...COMPONENTS
   ]
 })
