ce47499b248d62b8c085d1a654394ccf84adbe40
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-01-24 16:26:29 +0300

    add question name section and progress bar

--- src/app/main/questions/questions.module.ts
+++ src/app/main/questions/questions.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { RouterModule, Routes } from '@angular/router';
@@ -19,6 +20,7 @@ const routes: Routes = [
 const COMPONENT = [QuestionsComponent];
 
 const MATERIAL_MODULES = [
+  MatProgressBarModule,
   MatCardModule,
   MatButtonModule,
   MatProgressSpinnerModule,
