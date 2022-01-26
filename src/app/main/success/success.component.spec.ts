97febc84e16a5679abbde906021c5723e67fd4e9
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-01-26 15:47:37 +0300

    second test for success page - render mat-card

--- src/app/main/success/success.component.spec.ts
+++ src/app/main/success/success.component.spec.ts
@@ -20,4 +20,16 @@ describe('SuccessComponent', () =>
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it('SuccessComponent should has a mat-card and a div with class m-success-card-content in it', () =>
+  {
+    const fixture = TestBed.createComponent(SuccessComponent);
+    fixture.detectChanges();
+
+    const card = fixture.debugElement.nativeElement.querySelector('mat-card');
+
+    const content = card?.querySelector('div.m-success-card-content');
+
+    expect(content).not.toBeNull();
+  });
 });
