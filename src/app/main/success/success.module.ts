a540064fd4649017191512e5d68e49ee83df78e7
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-01-26 15:48:47 +0300

    render mat-card test in success page passed

--- src/app/main/success/success.module.ts
+++ src/app/main/success/success.module.ts
@@ -1,20 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule, Routes } from '@angular/router';
 import { SuccessComponent } from './success.component';
+
+const routes: Routes = [
+  { path: '', component: SuccessComponent }
+];
 
 const BASE_MODULES = [
   CommonModule
 ];
 const COMPONENTS = [SuccessComponent];
 
+const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatCardModule
+];
 @NgModule({
   declarations: [
     ...COMPONENTS
   ],
   imports: [
+    RouterModule.forChild(routes),
+    ...MATERIAL_MODULES,
     ...BASE_MODULES
   ],
   exports: [
+    ...MATERIAL_MODULES,
     ...BASE_MODULES,
     ...COMPONENTS
   ]
